5b9995beaf962618562f20aea3cbdf6893400617
LincMerwaneB <79836255+LincMerwaneB@users.noreply.github.com>
2022-09-07 14:12:32 +0200

    change space

--- setup.py
+++ setup.py
@@ -2,19 +2,22 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     """ Utility function to read the README file."""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 with open('requirements.txt') as file:
     required = file.read().splitlines()
 setup(
-    name = "servier_technical_test",
-    version = "0.0.1",
-    author = "Merwane Bouali",
-    author_email = "andrewjcarter@gmail.com",
-    description = ("a data pipeline that generates a graph from csv file"),
-    license = "BSD",
-    url = "http://packages.python.org/an_example_pypi_project",
+    name="servier_technical_test",
+    version="0.0.1",
+    author="Merwane Bouali",
+    author_email="andrewjcarter@gmail.com",
+    description=("a data pipeline that generates a graph from csv file"),
+    license="BSD",
+    url="http://packages.python.org/an_example_pypi_project",
     packages=['src', 'tests'],
     install_requires=required,
     long_description=read('README.md'),
