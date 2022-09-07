c15dc15fc26f42d504480d5d789ae2e5a3901292
LincMerwaneB <79836255+LincMerwaneB@users.noreply.github.com>
2022-09-07 12:02:40 +0200

    change for test

--- tests/test_graph_function.py
+++ tests/test_graph_function.py
@@ -1,4 +1,5 @@
 """ Unit test for graph_function module """
+from pathlib import Path
 import networkx as nx
 import pytest
 from src import import_data as imp
@@ -7,7 +8,8 @@ from src import graph_function as gr
 
 def test_graph_function():
     """ test the function test_graph_function graph type"""
-    df_drugs, df_journal = imp.import_dir_to_dataframe()
+    test_data = Path(Path(__file__).parent.resolve(), 'data')
+    df_drugs, df_journal = imp.import_dir_to_dataframe(test_data)
     graph = gr.create_graph(df_drugs, df_journal, show = False)
     assert isinstance(graph, nx.classes.digraph.DiGraph) is True
 
